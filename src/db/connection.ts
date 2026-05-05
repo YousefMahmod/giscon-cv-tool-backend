f776a9d3092da58120b1955b646a8037481dc63f
Youssef Mahmoud <youssef.shubra@yahoo.com>
2026-05-05 18:16:04 +0300

    update in connection too

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,7 +2,7 @@ import pkg from "pg";
 const { Pool } = pkg;
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./src/.env" });
+dotenv.config();
 
 const pool = new Pool({
   host: process.env.DB_HOST,
